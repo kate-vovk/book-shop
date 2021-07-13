aade8b309a0dbefd1424baddbd036e1a4ea79663
kate-vovk <kateryna.vovk@nure.ua>
2021-07-13 15:04:03 +0300

    feature/slick-slider

--- src/routes/Goods/components/Spinner/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-
-export const SpinnerContainertStyled = styled.div({
-  height: "100%",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-});
